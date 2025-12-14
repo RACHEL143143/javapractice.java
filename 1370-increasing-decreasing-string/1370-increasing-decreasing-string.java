8e8081caa345024c007e27df30443657b3e43363
Rachel <rachelrachel1400@gmail.com>
2025-12-14 23:38:12 +0530

    Time: 2 ms (100%), Space: 44.7 MB (47.96%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String sortString(String s){
+        int[] n=new int[26];
+        for(char ch:s.toCharArray()){
+        n[ch-'a']++;
+    }
+    StringBuilder sb=new StringBuilder();
+    while(sb.length()!=s.length()){
+        for(int i=0;i<26;i++){
+           if(n[i]!=0){
+              sb.append((char)(i+'a'));
+              n[i]--;
+           } 
+        }
+        for(int i=25;i>=0;i--){
+           if(n[i]!=0){
+            sb.append((char)(i+'a'));
+            n[i]--;
+           }
+        }
+    }
+    return sb.toString();
+
+    }
+
+}
